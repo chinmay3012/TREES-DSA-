b0034828ad70d442e990f314043cfe562729557d
Chinmay Mehrotra <mehrotrachinmay4@gmail.com>
2026-01-29 04:37:29 +0530

    LC-199 Binary Tree Right Side View

--- Trees/src/TreeBFS.java
+++ Trees/src/TreeBFS.java
@@ -126,6 +126,37 @@ public class TreeBFS {
             }
         }
 
+        //LC-199 Binary Tree Right Side View
+        class Solution {
+            public List<Integer> rightSideView(TreeNode root) {
+                List<Integer> res = new ArrayList<>();
+                if(root == null){
+                    return res;
+                }
+                Queue<TreeNode> queue = new LinkedList<>();
+                queue.offer(root);
+                while(!queue.isEmpty()){
+                    int levelSize = queue.size();
+
+                    for(int i=0;i<levelSize;i++) {
+                        TreeNode currNode = queue.poll();
+
+                        if(i == levelSize - 1){
+                            res.add(currLevel.val);
+                        }
+
+                        if (currNode.left != null) {
+                            queue.offer(currNode.left);
+                        }
+                        if (currNode.right != null) {
+                            queue.offer(currNode.right);
+                        }
+                    }
+                }
+                return res;
+            }
+        }
+
 
     }
 }
